d8813d6254d4a11bccaad19c855ddeb34aad5903
khushdeep <sukhijakhushdeep1@gmail.com>
2017-06-24 15:54:18 +0530

    updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,13 @@ class ArticlesController < ApplicationController
 @article = Article.new
 
   end
+  def index
+    @articles = Article.all
+
+  end
+  def edit
+@article = Article.find(params[:id])
+  end
 
   def create
     #render plain: params[:article].inspect
@@ -17,7 +24,16 @@ render 'new'
       end
       end
 
+def update
+  @article = Article.find(params[:id])
 
+  if @article.update(article_params)
+    flash[:notice] = "Article was successfully updated"
+    redirect_to article_path(@article)
+  else
+    render 'edit'
+  end
+end
 
 
 
